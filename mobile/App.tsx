6799564a80bb3dea1927d6bf72f0a3520562c4f4
Michel S. Stefani <michelstefani@empral.com.br>
2020-08-06 15:17:14 -0300

    Day 4 mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { AppLoading } from "expo";
+
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from "@expo-google-fonts/archivo";
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+
+import AppStack from "./src/routes/AppStack";
+
+export default function App() {
+  let [fontLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+}
---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+
+import styles from './styles';
+
+const Favorites: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos"/>
+    </View>
+  );
+}
+
+export default Favorites;
